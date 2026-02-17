a473c72bb19e47d293c6a9d495804c8db1c6bbe4
Ученик <ffkswfkfkwfe@**@e2>
2026-02-17 18:57:17 +0300

    Made

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import sys
 
 import requests
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel
 
-Shirota = "55.752004"
-Dolgota = "37.617734"
-Zoom = "15"
+Shirota = 55.752004
+Dolgota = 37.617734
+Zoom = 15
 
 
 class ApiMapsApp(QWidget):
+
     def __init__(self):
         super().__init__()
         self.map_label = None
+        self.shirota = Shirota
+        self.dolgota = Dolgota
+        self.zoom = Zoom
         self.initUI()
 
     def initUI(self):
@@ -24,8 +29,8 @@ class ApiMapsApp(QWidget):
     def get_map(self):
         api_server = "https://static-maps.yandex.ru/1.x/"
         params = {
-            "ll": f"{Dolgota},{Shirota}",
-            "z": Zoom,
+            "ll": f"{self.dolgota},{self.shirota}",
+            "z": str(Zoom),
             "l": "map"
         }
 
@@ -33,6 +38,22 @@ class ApiMapsApp(QWidget):
         pixmap = QPixmap()
         pixmap.loadFromData(response.content)
         self.map_label.setPixmap(pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_Up:
+            self.shirota += 0.01
+            self.get_map()
+        elif event.key() == Qt.Key.Key_Down:
+            self.shirota -= 0.01
+            self.get_map()
+        elif event.key() == Qt.Key.Key_Left:
+            self.dolgota -= 0.01
+            self.get_map()
+        elif event.key() == Qt.Key.Key_Right:
+            self.dolgota += 0.01
+            self.get_map()
+        else:
+            pass
 
 
 if __name__ == '__main__':

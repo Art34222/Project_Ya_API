c6eb5eaa39bb2786a8fb6c528299afb4886497a4
Artem <arrteem41@gmail.com>
2026-02-17 19:21:56 +0300

    Task 1-4

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sys
+
 import requests
-from PyQt6.QtGui import QPixmap, QKeyEvent
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QKeyEvent
 from PyQt6.QtGui import QPixmap
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel
-from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton
 
 Shirota = 55.752004
 Dolgota = 37.617734
@@ -17,7 +17,10 @@ class ApiMapsApp(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.theme_btn = None
         self.map_label = None
+        self.theme = "light"
+        self.api_key = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
         self.shirota = Shirota
         self.dolgota = Dolgota
         self.zoom = Zoom
@@ -29,12 +32,23 @@ class ApiMapsApp(QWidget):
         self.map_label = QLabel(self)
         self.get_map()
 
+        self.theme_btn = QPushButton("Сменить тему", self)
+        self.theme_btn.setGeometry(470, 410, 120, 30)
+        self.theme_btn.clicked.connect(self.toggle_theme)
+        self.theme_btn.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+
+    def toggle_theme(self):
+        self.theme = "dark" if self.theme == "light" else "light"
+        self.get_map()
+
     def get_map(self):
         api_server = "https://static-maps.yandex.ru/1.x/"
         params = {
             "ll": f"{self.dolgota},{self.shirota}",
             "z": str(Zoom),
-            "l": "map"
+            "l": "map",
+            "theme": self.theme,
+            "api_key": self.api_key
         }
 
         try:
